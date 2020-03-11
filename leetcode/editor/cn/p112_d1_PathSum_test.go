794b24a8bce047f4aeddbb920e7e8dce59d3c8aa
Bingo Huang <me@bingohuang.com>
2020-03-11 14:54:15 +0800

    add leetcode p112 test

---
+++ leetcode/editor/cn/p112_d1_PathSum_test.go
@@ -0,0 +1,135 @@
+// github.com/bingohuang/go-codes
+/**
+题目: 112. 路径总和
+难度: 1
+地址: https://leetcode-cn.com/problems/path-sum/
+*/
+package test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+// input and ouput
+type IO112 struct {
+	in1 *TreeNode
+	in2 int
+	out bool
+}
+
+func Test112(t *testing.T) {
+	// add test cases
+	treeNode_in1 := &TreeNode{
+		5,
+		&TreeNode{
+			4,
+			&TreeNode{
+				11,
+				&TreeNode{
+					7,
+					nil,
+					nil,
+				},
+				&TreeNode{
+					2,
+					nil,
+					nil,
+				},
+			},
+			nil,
+		},
+		&TreeNode{
+			8,
+			&TreeNode{
+				13,
+				nil,
+				nil,
+			},
+			&TreeNode{
+				4,
+				&TreeNode{
+					5,
+					nil,
+					nil,
+				},
+				&TreeNode{
+					1,
+					nil,
+					nil,
+				},
+			},
+		},
+	}
+	tc := map[string]IO112{
+		"1": IO112{treeNode_in1, 22, true},
+		"2": IO112{treeNode_in1, 100, false},
+	}
+
+	for k, v := range tc {
+		// algo func
+		out := hasPathSum(v.in1, v.in2)
+
+		fmt.Printf("case-%v:\n", k)
+		fmt.Printf("\tinput: %v\n", v.in1)
+		fmt.Printf("\toutput: %v\n", out)
+		fmt.Printf("\texcept: %v\n", v.out)
+
+		if !reflect.DeepEqual(out, v.out) {
+			t.Errorf("case-%v: except answer: [%v], get answer: [%v]", k, v.out, out)
+		}
+	}
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, sum int) bool {
+	var result bool
+	var pathValue int
+	var preOrder func(node *TreeNode)
+	preOrder = func(node *TreeNode) {
+		if result || node == nil {
+			return
+		}
+		pathValue += node.Val
+		if node.Left == nil && node.Right == nil && pathValue == sum {
+			result = true
+			return
+		}
+		preOrder(node.Left)
+		preOrder(node.Right)
+		pathValue -= node.Val
+	}
+	preOrder(root)
+	return result
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+/* 题目详情 */
+//给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+//
+// 说明: 叶子节点是指没有子节点的节点。
+//
+// 示例:
+//给定如下二叉树，以及目标和 sum = 22，
+//
+//               5
+//             / \
+//            4   8
+//           /   / \
+//          11  13  4
+//         /  \      \
+//        7    2      1
+//
+//
+// 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+// Related Topics 树 深度优先搜索
