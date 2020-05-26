5ca616c570f7f5cac7ccfdff2a67fa7b10ddb1d8
Bingo Huang <me@bingohuang.com>
2020-05-26 15:48:40 +0800

    update go-lang-test/sizeof

--- go-lang-test/sizeof/main.go
+++ go-lang-test/sizeof/main.go
@@ -23,9 +23,17 @@ func sizeof1() {
 		FieldD string
 	}{0, 0, 0, "bar"}
 
+	d := struct {
+		FieldA int8
+		FieldD string
+		FieldB float32
+		FieldC int64
+	}{0, "bar", 0, 0}
+
 	fmt.Printf("%d ", unsafe.Sizeof(a))
 	fmt.Printf("%d ", unsafe.Sizeof(b))
 	fmt.Printf("%d \n", unsafe.Sizeof(c))
+	fmt.Printf("%d \n", unsafe.Sizeof(d))
 }
 
 func sizeof2() {
@@ -35,4 +43,10 @@ func sizeof2() {
 	fmt.Printf("a: %T, %d\n", a, unsafe.Sizeof(a))
 	fmt.Printf("b: %T, %d\n", b, unsafe.Sizeof(b))
 	fmt.Printf("c: %T, %d\n", c, unsafe.Sizeof(c))
+
+	m := make(map[int]int)
+	s := []int{1}
+	fmt.Printf("m: %T, %d\n", m, unsafe.Sizeof(m))
+	fmt.Printf("s: %T, %d\n", s, unsafe.Sizeof(s))
+
 }
