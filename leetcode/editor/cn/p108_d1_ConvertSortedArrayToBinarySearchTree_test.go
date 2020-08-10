4ae96b4858efd002ba0ad05a09b69a2ba92f64d4
Bingo Huang <me@bingohuang.com>
2020-08-10 19:41:58 +0800

    add leetcode p108_d1_ConvertSortedArrayToBinarySearchTree_test

--- leetcode/editor/cn/p108_d1_ConvertSortedArrayToBinarySearchTree_test.go
+++ leetcode/editor/cn/p108_d1_ConvertSortedArrayToBinarySearchTree_test.go
@@ -1,8 +1,8 @@
 // github.com/bingohuang/go-codes
 /**
-题目: 面试题 04.02. 最小高度树
+题目: 108. 将有序数组转换为二叉搜索树
 难度: 1
-地址: https://leetcode-cn.com/problems/minimum-height-tree-lcci/
+地址: https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/
 */
 package test
 
@@ -13,14 +13,13 @@ import (
 )
 
 // input and ouput
-type IO面试题0402 struct {
+type IO108 struct {
 	in  []int
 	out *TreeNode
 }
 
-func Test面试题0402(t *testing.T) {
+func Test108(t *testing.T) {
 	// add test cases
-	// [0,-3,9,-10,null,5]
 	tc1_out := &TreeNode{
 		Val: 0,
 		Left: &TreeNode{
@@ -36,7 +35,8 @@ func Test面试题0402(t *testing.T) {
 			},
 		},
 	}
-	tc := map[string]IO面试题0402{
+	tc := map[string]IO108{
+		"0": {nil, nil},
 		"1": {[]int{-10, -3, 0, 5, 9}, tc1_out},
 	}
 
@@ -64,11 +64,12 @@ func Test面试题0402(t *testing.T) {
  *     Right *TreeNode
  * }
  */
-func sortedArrayToBST1(nums []int) *TreeNode {
-	// 20200727
-	// 1、递归法
-	// 执行耗时:8 ms,击败了9.61% 的Go用户
-	// 内存消耗:4.4 MB,击败了100.00% 的Go用户
+func sortedArrayToBST(nums []int) *TreeNode {
+	// 20200810
+	// 1、递归法：类似于面试题 04.02 求高度最小的二叉树
+	// 复杂度：O(N)/O(logN)
+	// 执行耗时:4 ms,击败了84.84% 的Go用户
+	// 内存消耗:4.4 MB,击败了79.79% 的Go用户
 	if len(nums) == 0 {
 		return nil
 	}
@@ -76,23 +77,30 @@ func sortedArrayToBST1(nums []int) *TreeNode {
 		return &TreeNode{Val: nums[0]}
 	}
 	middle := len(nums) / 2
-	head := &TreeNode{Val: nums[middle]}
-	head.Left = sortedArrayToBST(nums[:middle])
-	head.Right = sortedArrayToBST(nums[middle+1:])
-
-	return head
+	root := &TreeNode{Val: nums[middle]}
+	root.Left = sortedArrayToBST(nums[:middle])
+	root.Right = sortedArrayToBST(nums[middle+1:])
+	return root
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
 
 /* 题目详情 */
-//给定一个有序整数数组，元素各不相同且按升序排列，编写一个算法，创建一棵高度最小的二叉搜索树。
-//示例: 给定有序数组: [-10,-3,0,5,9], 一个可能的答案是：[0,-3,9,-10,null,5]，
-// 它可以表示下面这个高度平衡二叉搜索树：
-//	  0
-//	 / \
-//  -3  9
-//  /   /
+//将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+//
+// 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+//
+// 示例:
+//
+// 给定有序数组: [-10,-3,0,5,9],
+//
+//一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+//
+//      0
+//     / \
+//   -3   9
+//   /   /
 // -10  5
-//Related Topics 树 深度优先搜索
-// 👍 42 👎 0
+//
+// Related Topics 树 深度优先搜索
+// 👍 540 👎 0
