ebafbc221e1ccba4ea6103ddb095bdeadaf55f9b
Bingo Huang <me@bingohuang.com>
2020-09-04 17:34:44 +0800

    update leetcode p257_d1_BinaryTreePaths_test

--- leetcode/editor/cn/p257_d1_BinaryTreePaths_test.go
+++ leetcode/editor/cn/p257_d1_BinaryTreePaths_test.go
@@ -43,6 +43,7 @@ func Test257(t *testing.T) {
 		},
 	}
 	tc := map[string]IO257{
+		"0": {nil, nil},
 		"1": {tc1_in, []string{"1->2", "1->3"}},
 		"2": {tc2_in, []string{"1->2->5", "1->3"}},
 	}
@@ -98,7 +99,7 @@ func binaryTreePaths(root *TreeNode) []string {
 	// 1、迭代法
 	// 执行耗时:0 ms,击败了100.00% 的Go用户
 	// 内存消耗:2.4 MB,击败了100.00% 的Go用户
-	if root == nil {
+	/*if root == nil {
 		return nil
 	}
 	paths := make([]string, 0)
@@ -121,7 +122,64 @@ func binaryTreePaths(root *TreeNode) []string {
 			pathStack = append(pathStack, path+"->"+strconv.Itoa(node.Right.Val))
 		}
 	}
-	return paths
+	return paths*/
+
+	// 2020-09-04 16:38 @bingohuang 每日一题回顾
+	// 算法：1、前序遍历、递归法
+	// 复杂度：O(N^2)/O(N^2)
+	// 效率：执行耗时:0 ms,击败了100.00% 的Go用户
+	//			内存消耗:2.4 MB,击败了35.94% 的Go用户
+	/*var res []string
+	var preorder func(node *TreeNode, path string)
+	preorder = func(node *TreeNode, path string) {
+		if node == nil {
+			return
+		}
+		path += strconv.Itoa(node.Val)
+		if node.Left == nil && node.Right == nil {
+			res = append(res, path)
+			return
+		}
+		path += "->"
+		if node.Left != nil {
+			preorder(node.Left, path)
+		}
+		if node.Right != nil {
+			preorder(node.Right, path)
+		}
+	}
+	preorder(root, "")
+	return res*/
+
+	// 2020-09-04 17:02 @bingohuang
+	// 算法：2、迭代法
+	// 复杂度：O(N^2)/O(N^2)
+	// 效率：执行耗时:4 ms,击败了11.86% 的Go用户
+	//			内存消耗:2.4 MB,击败了31.25% 的Go用户
+	if root == nil {
+		return nil
+	}
+	var res []string
+	nodes := []*TreeNode{root}
+	paths := []string{strconv.Itoa(root.Val)}
+	for len(nodes) > 0 {
+		node := nodes[0]
+		nodes = nodes[1:]
+		path := paths[0]
+		paths = paths[1:]
+		if node.Left == nil && node.Right == nil {
+			res = append(res, path)
+		}
+		if node.Left != nil {
+			nodes = append(nodes, node.Left)
+			paths = append(paths, path+"->"+strconv.Itoa(node.Left.Val))
+		}
+		if node.Right != nil {
+			nodes = append(nodes, node.Right)
+			paths = append(paths, path+"->"+strconv.Itoa(node.Right.Val))
+		}
+	}
+	return res
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
